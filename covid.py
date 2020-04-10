8fb80d1b9b05fb661955686da204579935138627
swapnilkathane <62869345+swapnilkathane@users.noreply.github.com>
2020-04-11 03:23:04 +0530

    telegrambot

--- covid.py
+++ covid.py
@@ -2,6 +2,22 @@ import time, datetime
 import telepot
 import requests
 import json
+import vid
+from bs4 import BeautifulSoup as bs
+
+
+response = requests.get("https://www.indiatoday.in/coronavirus")
+response2=requests.get("https://lokmat.news18.com/tag/corona-virus/")
+soup=bs(response.text, "html.parser")
+soup2=bs(response2.text, "html.parser")
+#print(soup.prettify())
+latest_news=soup.find_all('div', class_='pollution-right')
+latest_news2=soup2.find('div', class_="photo-story")
+op=(latest_news[0].text )
+op2=(latest_news[1].text)
+op3=latest_news2.text
+
+
 from telepot.loop import MessageLoop
 
 now = datetime.datetime.now()
@@ -33,15 +49,17 @@ def action(msg):
     print('Received: %s', command)
     if command.casefold() == 'hi':
         telegram_bot.sendMessage(chat_id,
-                                 str("Hi! Welcome to covid19 Maharashtra info and help bot use /help for more info "))
+                                 str("Hi! Welcome to covid-19 Maharashtra. \n I am here to tell you number of active cases in your city. \n  To start with it please enter /start \nTo know more about me enter  /help "))
+    elif command == "/news":
+        telegram_bot.sendMessage(chat_id,str(op ) + str("\n") + str("\n")+ str(op2) + str( "\n source :-IndiaToday")  + str("\n")+ str(op3)+ str( "\n source :-IBN-LOKMAT"))
     elif command == '/start':
         telegram_bot.sendMessage(chat_id,
-                                 str("Welcome to covid 19 Maharashtra information bot type  /help  for more info "))
+                             str("Enter you city name to know, \n number of active cases"))
     elif command == 'who are you ?':
         telegram_bot.sendMessage(chat_id, str("I'm covid 19 help Bot created by Pankaj and Swapnil "))
     elif command == '/help':
         telegram_bot.sendMessage(chat_id, str(
-            "I'm covid 19 help Bot \n for now I can answer following commands \n hi /start\n asking active patient in any city just send city name eg. Mumbai"))
+            "I'm in my devlopment phase. \n I am responsive only to /start, /help, /hi, /news and who are you ? \n Please bear with me." ))
     else:
         print(command)
         print(chat_id)
@@ -49,8 +67,8 @@ def action(msg):
         if (Number > -1):
             telegram_bot.sendMessage(chat_id, str("Active patient in ") + str(command) + str(" = ") + str(Number))
         else:
-            telegram_bot.sendMessage(chat_id, str("Sorry I dont have any info about ") + str(command) + str(
-                "  as i am in testing phase please bear with me  "))
+            telegram_bot.sendMessage(chat_id, str("Sorry I don't have any information about ") + str(command) + str(
+                " Please visit /help, Thank you! "))
 
 
 telegram_bot = telepot.Bot('')
@@ -59,3 +77,7 @@ MessageLoop(telegram_bot, action).run_as_thread()
 print('Up and Running....')
 while 1:
     time.sleep(10)
+
+
+
+
